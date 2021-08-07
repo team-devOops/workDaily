c88a0d14fa6cf19958390ee283e3ca8df317681b
kbh0581 <kbh0581@gmail.com>
2021-08-07 23:53:23 +0900

    init

---
+++ src/main/java/com/devOops/workDaily/WorkDailyApplication.java
@@ -0,0 +1,13 @@
+package com.devOops.workDaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkDailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkDailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devOops/workDaily/WorkDailyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devOops.workDaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkDailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
